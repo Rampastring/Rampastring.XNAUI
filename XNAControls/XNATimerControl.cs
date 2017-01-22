87612497b9999910700bcd9f1ff4e3f9afc35634
Rampastring <rampastring@outlook.com>
2017-01-22 10:37:01 +0200

    Added XNATimerControl

---
+++ XNAControls/XNATimerControl.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Rampastring.XNAUI.XNAControls
+{
+    /// <summary>
+    /// A control that acts as a timer.
+    /// </summary>
+    public class XNATimerControl : XNAControl
+    {
+        public event EventHandler TimeElapsed;
+
+        public XNATimerControl(WindowManager windowManager) : base(windowManager)
+        {
+            Visible = false;
+        }
+
+        private TimeSpan _interval;
+        public TimeSpan Interval
+        {
+            get { return _interval; }
+            set { _interval = value; }
+        }
+
+        /// <summary>
+        /// If set, the timer will automatically restart after calling
+        /// the TimeElapsed event.
+        /// </summary>
+        public bool AutoReset { get; set; }
+
+        private TimeSpan currentInterval;
+        private bool timerEnabled;
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (!timerEnabled)
+                return;
+
+            currentInterval -= gameTime.ElapsedGameTime;
+
+            if (currentInterval < TimeSpan.Zero)
+            {
+                TimeElapsed?.Invoke(this, EventArgs.Empty);
+
+                if (AutoReset)
+                    currentInterval = Interval;
+            }
+        }
+
+        /// <summary>
+        /// Pauses the timer.
+        /// </summary>
+        public void Pause()
+        {
+            timerEnabled = false;
+        }
+
+        /// <summary>
+        /// Resumes a previously paused (with Pause()) timer.
+        /// </summary>
+        public void Resume()
+        {
+            timerEnabled = true;
+        }
+
+        /// <summary>
+        /// Sets the timer to trigger after the time specified in the timer's Interval 
+        /// and starts the timer.
+        /// </summary>
+        public void Start()
+        {
+            currentInterval = Interval;
+            timerEnabled = true;
+        }
+
+        /// <summary>
+        /// Lowers the time until the next event by the specified amount of time.
+        /// </summary>
+        /// <param name="timeSpan">The time.</param>
+        public void Accelerate(TimeSpan timeSpan)
+        {
+            currentInterval -= timeSpan;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            // Do nothing
+        }
+    }
+}
