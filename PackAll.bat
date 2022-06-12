62ae4c2a9cda181ea75533d3ce4ec6c74a446f0e
Rans4ckeR <ransacker@outlook.com>
2022-06-12 16:30:54 +0200

    Generate Debug packages

--- PackAll.bat
+++ PackAll.bat
@@ -1,3 +1,7 @@
 dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXRelease
 dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLRelease
 dotnet pack .\Rampastring.XNAUI.csproj -c XNARelease -p:PlatformTarget=x86
+
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXDebug
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLDebug
+dotnet pack .\Rampastring.XNAUI.csproj -c XNADebug -p:PlatformTarget=x86
