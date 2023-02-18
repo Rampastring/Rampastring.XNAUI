b04c9f9f28014ef7503313d6f40ebaedab347d70
Rans4ckeR <ransacker@outlook.com>
2023-02-19 00:22:55 +0100

    Fix MSB3270: There was a mismatch between the processor architecture of the project being built and the processor architecture of the reference

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,4 +1,9 @@
 dotnet build .\Rampastring.XNAUI.csproj -c WindowsDXRelease
 dotnet build .\Rampastring.XNAUI.csproj -c UniversalGLRelease
 dotnet build .\Rampastring.XNAUI.csproj -c WindowsGLRelease
-dotnet build .\Rampastring.XNAUI.csproj -c WindowsXNARelease
+dotnet build .\Rampastring.XNAUI.csproj -c WindowsXNARelease --runtime win-x86 --no-self-contained
+
+dotnet build .\Rampastring.XNAUI.csproj -c WindowsDXDebug
+dotnet build .\Rampastring.XNAUI.csproj -c UniversalGLDebug
+dotnet build .\Rampastring.XNAUI.csproj -c WindowsGLDebug
+dotnet build .\Rampastring.XNAUI.csproj -c WindowsXNADebug --runtime win-x86 --no-self-contained
