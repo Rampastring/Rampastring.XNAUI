ea4f6d844c994d6cace12de37a0577a41fddf1eb
Rampastring <rampastring@outlook.com>
2023-03-02 19:19:31 +0200

    Attach detached control when it is killed

--- XNAControls/XNAControl.cs
+++ XNAControls/XNAControl.cs
@@ -1100,6 +1100,9 @@ public class XNAControl : DrawableGameComponent
     /// </summary>
     public virtual void Kill()
     {
+        if (Detached)
+            Attach();
+
         var childrenCopy = new List<XNAControl>(Children);
         childrenCopy.ForEach(c => c.Kill());
         childrenCopy.ForEach(RemoveChild);
