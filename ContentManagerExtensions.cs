28dfe298683051ba69c6ff8cffd133ffd4637194
[11EJ11] <jacobseliot54@gmail.com>
2025-08-11 16:23:17 +1200

    Add FontType enum Extract ContentManager root directory logic into extension method

---
+++ ContentManagerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Content;
+#if XNA
+using System.Reflection;
+#endif
+
+namespace Rampastring.XNAUI
+{
+    public static class ContentManagerExtensions
+    {
+#if XNA
+        private static readonly FieldInfo rootDirectoryField = typeof(ContentManager)
+            .GetField("rootDirectory", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.SetField | BindingFlags.GetField);
+        private static readonly FieldInfo fullRootDirectoryField = typeof(ContentManager)
+            .GetField("fullRootDirectory", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.SetField | BindingFlags.GetField);
+#endif
+
+        /// <summary>
+        /// Sets the root directory for the ContentManager with XNA vs MonoGame differences.
+        /// </summary>
+        /// <param name="contentManager">The ContentManager instance.</param>
+        /// <param name="rootDirectory">The new root directory path.</param>
+        public static void SetRootDirectory(this ContentManager contentManager, string rootDirectory)
+        {
+#if !XNA
+            contentManager.RootDirectory = rootDirectory;
+#else
+            rootDirectoryField.SetValue(contentManager, rootDirectory);
+            fullRootDirectoryField.SetValue(contentManager, rootDirectory);
+#endif
+        }
+    }
+}
